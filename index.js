8d8eed66ed04cb2de5484abb1f4261f020d8a153
kom7904 <kom7902@gmail.com>
2020-08-08 13:16:36 +0900

    bot upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on('message', (message) => {
+  if(message.content === '병신') {
+    message.reply('왜불러~');
+  }
+});
+
+client.login(token);
